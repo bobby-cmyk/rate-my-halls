752ec3b93020b0e01e182492c5f582544fc7c5cf
bobby-cmyk <92661997+bobby-cmyk@users.noreply.github.com>
2023-05-21 16:07:26 +0000

    Added html and css and api

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+REVIEWS = [{
+  'id': 1,
+  'hall': "Kent Ridge Hall",
+  'rating': '4/5',
+  'title': "Amazing Hall",
+  "content": "Great location! Really near to the NUS Business School"
+}, {
+  'id': 2,
+  'hall': "Raffles Hall",
+  'rating': '4.5/5',
+  'title': "Great Experience",
+  "content": "Good facilities. Gym newly renovated!"
+}, {
+  'id': 3,
+  'hall': "Temasek Hall",
+  'rating': '3.8/5',
+  'title': "Really bad experience",
+  "content": "Near to supper streatch. Amazing CCAs"
+}]
+
 
 @app.route("/")
-def hello_world():
-  return "Hello Aiken!"
+def main_page():
+  return render_template('home.html', reviews=REVIEWS)
+
+
+@app.route("/api/reviews")
+def list_reviews():
+  return jsonify(REVIEWS)
 
 
 #Running locally, debug = true
