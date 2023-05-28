51ff0fb41e7447a19c808a55f9cc8f1c0574f173
bobby-cmyk <92661997+bobby-cmyk@users.noreply.github.com>
2023-05-28 16:56:39 +0000

    added individual hall and review

--- app.py
+++ app.py
@@ -1,19 +1,53 @@
 from flask import Flask, render_template, jsonify
-from database import load_reviews_from_db
+from database import load_reviews_from_db, load_review_from_db, load_halls_from_db, load_hall_from_db
 
 app = Flask(__name__)
 
 
+#All Reviews
 @app.route("/")
-def main_page():
+def all_reviews():
   reviews = load_reviews_from_db()
   return render_template('home.html', reviews=reviews)
 
 
+#All Reviews (API)
 @app.route("/api/reviews")
-def list_reviews():
+def api_all_reviews():
   reviews = load_reviews_from_db()
   return jsonify(reviews)
+
+
+#Individual Review
+@app.route("/reviews/<id>")
+def review(id):
+  review = load_review_from_db(id)
+  if not review:
+    return "Page Not Found", 404
+  return render_template('reviewpage.html', review=review)
+
+
+#All Halls
+@app.route("/halls")
+def all_halls():
+  halls = load_halls_from_db()
+  return render_template('halls.html', halls=halls)
+
+
+#All Halls
+@app.route("/api/halls")
+def api_all_halls():
+  halls = load_halls_from_db()
+  return jsonify(halls)
+
+
+#Individual Hall
+@app.route("/halls/<id>")
+def hall(id):
+  hall = load_hall_from_db(id)
+  if not hall:
+    return "Page Not Found", 404
+  return render_template('hallpage.html', hall=hall)
 
 
 #Running locally, debug = true
--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
 import os
 
+#Authentication & Setting Up Connection
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string,
@@ -9,12 +10,57 @@ engine = create_engine(db_connection_string,
                        }})
 
 
+#Load all reviews
 def load_reviews_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from reviews"))
+    result = conn.execute(
+      text(
+        "SELECT * FROM reviews LEFT JOIN halls ON reviews.hallId=halls.hallId;"
+      ))
     result_all = result.all()
     column_name = result.keys()
     reviews = []
     for row in result_all:
       reviews.append(dict(zip(column_name, row)))
     return reviews
+
+
+#Load a review based on id
+def load_review_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"SELECT * FROM reviews WHERE reviewId={id}"))
+    rows = []
+    column_name = result.keys()
+    for row in result.all():
+      rows.append(dict(zip(column_name, row)))
+
+    if len(rows) == 0:
+      return None
+    else:
+      return (rows[0])
+
+
+#Load all halls
+def load_halls_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM halls"))
+    result_all = result.all()
+    column_name = result.keys()
+    halls = []
+    for row in result_all:
+      halls.append(dict(zip(column_name, row)))
+    return halls
+
+#Load a hall details
+def load_hall_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"SELECT * FROM halls WHERE hallId={id}"))
+    rows = []
+    column_name = result.keys()
+    for row in result.all():
+      rows.append(dict(zip(column_name, row)))
+
+    if len(rows) == 0:
+      return None
+    else:
+      return (rows[0])
